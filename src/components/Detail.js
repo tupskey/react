a5931123cdf3b4483a2821ce93041ad35b79ca33
Olatunde Temitope <tupskey@gmail.com>
2019-03-28 09:52:41 +0100

    thunk

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, BreadcrumbItem, Breadcrumb, Button,Modal, ModalHeader, ModalBody, Row, Label, Col } from 'reactstrap';
 import {Link} from 'react-router-dom';
 import { LocalForm, Errors, Control} from 'react-redux-form';
+import { Loading} from './LoadingComponent';
 
 
   const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -139,7 +140,25 @@ import { LocalForm, Errors, Control} from 'react-redux-form';
 
 
      const DishDetail = (props) => {
-      if(props.dish != null)
+      if (props.isLoading) {
+        return(
+            <div className="container">
+              <div className="row">
+                <Loading />
+              </div>
+            </div>
+          );
+      }
+      else if (props.errMess) {
+        return (
+            <div className="container"> 
+              <div className="row">
+                <h4>{props.errMess}</h4>
+              </div>
+            </div>
+          );
+      }
+      else if(props.dish != null)
         return(
           <div className="container">
             <div className="row">
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,21 +1,23 @@
-import { createStore, combineReducers} from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import {Dishes} from './dishes';
 import {Leaders} from './leaders';
 import {Comments} from './comments';
 import {Promotions} from './promotions';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 export const ConfigureStore = () => {
-	const store = createStore(
+	const store = createStore( 
 		combineReducers({
 			dishes: Dishes,
 			leaders: Leaders,
 			comments: Comments,
 			promotions: Promotions
-		})
+		}),
+		applyMiddleware(thunk, logger)
 		);
 	
 	return store;
 }
-
 
 
